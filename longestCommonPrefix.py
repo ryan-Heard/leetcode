af2f0049d2f4c04387f5d7aa7934c6a5688ea3fd
Ryan Heard <brotherakuma@gmail.com>
2017-09-27 20:51:03 -0500

    Created simple program

--- longestCommonPrefix.py
+++ longestCommonPrefix.py
@@ -1,3 +1,6 @@
+from collections import OrderedDict
+
+
 class Solution(object):
     def longestCommonPrefix(self, strs):
         """
@@ -6,3 +9,21 @@ class Solution(object):
         :type strs: List[str]
         :rtype: str
         """
+        if strs == []:
+            return ""
+        # cycle through each array checking the letter
+        # if one letter doesn't match then look isn't true
+
+        # Zip to unpack all the letters at once
+        # Enumerate allows you to find the i value
+        for i, letter_group in enumerate(zip(*strs)):
+            print(letter_group)
+            if len(set(letter_group)) > 1:
+                return strs[0][:i]
+        else:
+            return min(strs)
+
+
+if __name__ == '__main__':
+    test = ['abc', 'abcd', 'abcde']
+    print(Solution().longestCommonPrefix(test))
